5c8e540010832503f3f7f488362165d175c85490
呉真 <kuretru@gmail.com>
2021-04-28 21:15:24 +0800

    新增[Web]：载入仅疫苗数据

--- covid19-vaccinations/src/components/WorldMap/WorldMap.tsx
+++ covid19-vaccinations/src/components/WorldMap/WorldMap.tsx
@@ -10,30 +10,46 @@ const WorldMap = () => {
     "https://cdn.jsdelivr.net/gh/owid/covid-19-data/public/data/vaccinations/vaccinations.csv";
 
   const country_data = new Map();
+  const max_data = {};
 
   const [myState, setMyState] = useState<Boolean>(true);
   const ref: RefObject<HTMLDivElement> = React.createRef();
 
   const fetch_data = () => {
-    d3.csv(COUNTRY_DATA).then((data: any) => {
-      data.forEach((element: any) => {
-        const iso: string = element.iso_code;
-        if (!country_data.has(iso)) {
-          country_data.set(iso, { information: null, vaccination: [] });
-        }
-        country_data.get(iso).information = element;
+    const fetch = Promise.all([
+      d3.csv(COUNTRY_DATA).then((data: any) => {
+        data.forEach((element: any) => {
+          const iso: string = element.iso_code;
+          if (!country_data.has(iso)) {
+            country_data.set(iso, {
+              iso: iso,
+              information: null,
+              vaccination: [],
+            });
+          }
+          country_data.get(iso).information = element;
+        });
+        console.log("country done");
+      }),
+      d3.csv(VACCINATION_DATA).then((data: any) => {
+        data.forEach((element: any) => {
+          const iso: string = element.iso_code;
+          if (!country_data.has(iso)) {
+            country_data.set(iso, {
+              iso: iso,
+              information: null,
+              vaccination: [],
+            });
+          }
+          country_data.get(iso).vaccination.push(element);
+        });
+        console.log("vaccination done");
+      }),
+    ]).then(() => {
+      console.log("ok");
+      country_data.forEach((element: any) => {
+        console.log(element);
       });
-      console.log("country done");
-    });
-    d3.csv(VACCINATION_DATA).then((data: any) => {
-      data.forEach((element: any) => {
-        const iso: string = element.iso_code;
-        if (!country_data.has(iso)) {
-          country_data.set(iso, { information: null, vaccination: [] });
-        }
-        country_data.get(iso).vaccination.push(element);
-      });
-      console.log("vaccination done");
     });
   };
 
@@ -45,8 +61,9 @@ const WorldMap = () => {
   const draw = () => {
     const width = 800;
     const height = 500;
-
     const svg = d3.select("svg").attr("width", width).attr("height", height);
+
+    const color = d3.scaleQuantize([1, 10], d3.schemeBlues[9]);
 
     const projection = d3.geoMercator().center([0, 5]).scale(150);
     const path = d3.geoPath().projection(projection);
@@ -61,14 +78,22 @@ const WorldMap = () => {
         .join((enter) => {
           const p = enter.append("path");
           p.on("mouseenter", (e, d: any) => {
-            console.log(d.properties.ADMIN);
+            console.log(d);
             d3.select(d).raise();
           });
           p.append("title");
           return p;
         })
         .attr("d", (d: any) => path(d))
-        .style("fill", (d: any) => "#123456");
+        .style("fill", (d: any) => {
+          const iso: string = d.properties.ISO_A3;
+          if (country_data.has(iso)) {
+            const latest = country_data.get(iso).vaccination.slice(-1)[0];
+            // return color(latest.total_vaccinations);
+            return color(4);
+          }
+          return color(0);
+        });
     });
   };
 
