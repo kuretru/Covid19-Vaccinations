40e4c7a8286633da57a6c3ac486da37cd1c956a0
呉真 <kuretru@gmail.com>
2021-04-30 01:21:09 +0800

    新增[Web]：菜单选项

--- covid19-vaccinations/src/App.tsx
+++ covid19-vaccinations/src/App.tsx
@@ -3,40 +3,71 @@ import logo from "./logo.svg";
 import "./App.css";
 import WorldMap from "./components/WorldMap/WorldMap";
 import { Layout, Menu } from "antd";
-import { UserAddOutlined, SmileOutlined } from "@ant-design/icons";
+import {
+  ReconciliationOutlined,
+  UserAddOutlined,
+  UserDeleteOutlined,
+  MedicineBoxOutlined,
+  PushpinOutlined,
+  SmileOutlined,
+  PaperClipOutlined,
+  TeamOutlined,
+  PayCircleOutlined,
+  PercentageOutlined,
+  ContactsOutlined,
+} from "@ant-design/icons";
 import SubMenu from "antd/lib/menu/SubMenu";
 
 const { Header, Footer, Sider, Content } = Layout;
 
 const App: FC = () => (
   <div className="App">
-    <Layout>
+    <Layout style={{ height: "100vh" }}>
       <Header className="header">
         <div className="logo">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <h1>COVID-19疫苗数据可视化</h1>
+        <h1>COVID-19数据可视化分析框架</h1>
       </Header>
+
       <Content style={{ padding: "25px 25px 0 25px" }}>
-        <Layout className="site-layout-background" style={{ padding: "24px 0" }}>
+        <Layout className="site-layout-background" style={{ padding: "24px 0", height: "100%" }}>
           <Sider width={180}>
             <Menu
               mode="inline"
-              defaultOpenKeys={["vaccinated"]}
-              defaultSelectedKeys={["vaccineDoses"]}
+              defaultOpenKeys={["vaccine"]}
+              defaultSelectedKeys={["vaccinations"]}
               style={{ height: "100%" }}
             >
-              <SubMenu key="vaccinated" title="疫苗数据">
-                <Menu.Item key="vaccineDoses" icon={<UserAddOutlined />}>
-                  疫苗注射情况
+              <SubMenu key="epidemic" title="疫情数据" icon={<ReconciliationOutlined />}>
+                <Menu.Item key="confirmed" icon={<UserAddOutlined />}>
+                  确诊数据
                 </Menu.Item>
-                <Menu.Item key="2" icon={<SmileOutlined />}>
-                  完成注射
+                <Menu.Item key="deaths" icon={<UserDeleteOutlined />}>
+                  致死数据
                 </Menu.Item>
               </SubMenu>
-              <SubMenu key="confirmed" title="确诊数据">
-                <Menu.Item key="3">疫苗数据</Menu.Item>
-                <Menu.Item key="4">确诊数据</Menu.Item>
+              <SubMenu key="vaccine" title="疫苗数据" icon={<MedicineBoxOutlined />}>
+                <Menu.Item key="vaccinations" icon={<PushpinOutlined />}>
+                  接种数据
+                </Menu.Item>
+                <Menu.Item key="fully_vaccinated" icon={<SmileOutlined />}>
+                  完成接种数据
+                </Menu.Item>
+              </SubMenu>
+              <SubMenu key="others" title="其他数据" icon={<PaperClipOutlined />}>
+                <Menu.Item key="population" icon={<TeamOutlined />}>
+                  人口数据
+                </Menu.Item>
+                <Menu.Item key="median_age" icon={<PayCircleOutlined />}>
+                  平均年龄
+                </Menu.Item>
+                <Menu.Item key="gdp" icon={<PercentageOutlined />}>
+                  人均GDP
+                </Menu.Item>
+                <Menu.Item key="life_expectancy" icon={<ContactsOutlined />}>
+                  平均寿命
+                </Menu.Item>
               </SubMenu>
             </Menu>
           </Sider>
